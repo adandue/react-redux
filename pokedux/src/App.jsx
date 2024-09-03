4b447f198053d04902e2346c79c67b407bd66cef
adan_due <ing.adanduenas@gmail.com>
2024-09-03 10:50:28 -0600

    Integrando Redux

--- pokedux/src/App.jsx
+++ pokedux/src/App.jsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from 'react'
+import { connect } from 'react-redux'
 import { Col } from 'antd'
 import { Searcher } from './Components/Searcher'
 import PokemonList from './Components/PokemonList'
 import { getPokemon } from './api'
+import { setPokemons as setPokemonsActions } from './actions'
 import './App.css'
 
-function App() {
+function App({ pokemons, setPokemons }) {
 const logo = 'https://static.platzi.com/media/tmp/class-files/github/curso-redux/curso-redux-01-pokeapi/src/statics/logo.svg'
 
-const [pokemons, setPokemons] = useState([])
 
 useEffect(() => {
   const fetchPokemons = async () => {
@@ -32,4 +33,12 @@ useEffect(() => {
   )
 }
 
-export default App
+const mapStateToProps = (state) => ({
+  pokemons: state.pokemons,
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  setPokemons: (value) => dispatch(setPokemonsActions(value))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- pokedux/src/main.jsx
+++ pokedux/src/main.jsx
@@ -1,7 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { pokemonsReducer } from './reducers/pokemons.jsx'
+import { Provider } from 'react-redux'
+import { legacy_createStore as createStore } from 'redux'
 import './index.css'
 
+const store = createStore(pokemonsReducer)
+
 createRoot(document.getElementById('root')).render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 )
