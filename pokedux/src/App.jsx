9be6c66ae7b9f39ebc2c35d2fa8b2a97f850277a
adan_due <ing.adanduenas@gmail.com>
2024-09-03 09:26:52 -0600

    Se pone el logo en la página principal

--- pokedux/src/App.jsx
+++ pokedux/src/App.jsx
@@ -4,9 +4,13 @@ import PokemonList from './Components/PokemonList'
 import './App.css'
 
 function App() {
+const logo = 'https://static.platzi.com/media/tmp/class-files/github/curso-redux/curso-redux-01-pokeapi/src/statics/logo.svg'
 
   return (
     <div className='App'>
+      <Col span={4} offset={10}>
+        <img src={logo} alt='Pokedux'/>
+      </Col>
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
