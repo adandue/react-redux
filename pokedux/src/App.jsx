583ecacb6e6316453a8bf6602620638b8fc3f0c1
adan_due <ing.adanduenas@gmail.com>
2024-09-03 11:05:40 -0600

    Hooks vs Connect

--- pokedux/src/App.jsx
+++ pokedux/src/App.jsx
@@ -1,20 +1,24 @@
-import { useEffect, useState } from 'react'
-import { connect } from 'react-redux'
+import { useEffect } from 'react'
 import { Col } from 'antd'
 import { Searcher } from './Components/Searcher'
 import PokemonList from './Components/PokemonList'
 import { getPokemon } from './api'
-import { setPokemons as setPokemonsActions } from './actions'
+import { setPokemons } from './actions'
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
 
-function App({ pokemons, setPokemons }) {
+function App() {
 const logo = 'https://static.platzi.com/media/tmp/class-files/github/curso-redux/curso-redux-01-pokeapi/src/statics/logo.svg'
 
+const pokemons = useSelector(state => state.pokemons)
+const dispatch = useDispatch()
 
 useEffect(() => {
+
+
   const fetchPokemons = async () => {
     const pokemonsRes = await getPokemon()
-    setPokemons(pokemonsRes)
+    dispatch(setPokemons(pokemonsRes))
   }
 
   fetchPokemons()
@@ -33,12 +37,6 @@ useEffect(() => {
   )
 }
 
-const mapStateToProps = (state) => ({
-  pokemons: state.pokemons,
-})
 
-const mapDispatchToProps = (dispatch) => ({
-  setPokemons: (value) => dispatch(setPokemonsActions(value))
-})
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
