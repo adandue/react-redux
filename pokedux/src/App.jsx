f8e6169ea0783823042d063ab961c6849d6e430b
adan_due <ing.adanduenas@gmail.com>
2024-09-05 12:30:27 -0600

    Agregando el loader

--- pokedux/src/App.jsx
+++ pokedux/src/App.jsx
@@ -1,8 +1,8 @@
 import { useEffect } from 'react'
-import { Col } from 'antd'
+import { Col, Spin } from 'antd'
 import { Searcher } from './Components/Searcher'
 import { getPokemon, getPokemonDetails } from './api'
-import { getPokemonsWithDetails, setPokemons } from './actions'
+import { getPokemonsWithDetails, setLoading, setPokemons } from './actions'
 import { useDispatch, useSelector } from 'react-redux'
 import PokemonList from './Components/PokemonList'
 import './App.css'
@@ -11,14 +11,17 @@ function App() {
 const logo = 'https://static.platzi.com/media/tmp/class-files/github/curso-redux/curso-redux-01-pokeapi/src/statics/logo.svg'
 
 const pokemons = useSelector(state => state.pokemons)
+const loading = useSelector(state => state.loading)
 const dispatch = useDispatch()
 
 useEffect(() => {
 
 
   const fetchPokemons = async () => {
+    dispatch(setLoading(true))
     const pokemonsRes = await getPokemon()
     dispatch(getPokemonsWithDetails(pokemonsRes))
+    dispatch(setLoading(false))
   }
 
   fetchPokemons()
@@ -32,7 +35,14 @@ useEffect(() => {
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
-      <PokemonList pokemons={pokemons} />
+      {
+        loading ? (
+          <Col offset={12}>
+            <Spin spinning size='large' />
+      </Col>
+      ) : (
+        <PokemonList pokemons={pokemons} />
+      )}
     </div>
   )
 }
--- pokedux/src/main.jsx
+++ pokedux/src/main.jsx
@@ -7,9 +7,9 @@ import App from './App.jsx'
 import './index.css'
 
 const composeAlt = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const composedEnhancers = compose(applyMiddleware(thunk))
+const composedEnhancers = composeAlt(applyMiddleware(thunk))
 
-const store = createStore(pokemonsReducer, window.REDUX_DEVTOOLS_EXTENSION && window.REDUX_DEVTOOLS_EXTENSION(), composedEnhancers)
+const store = createStore(pokemonsReducer, composedEnhancers)
 
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
